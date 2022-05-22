acf62fe859ef19c341e7e603967a07ffa2d76aaa
Lokya Naik <lokya57@gmail.com>
2022-05-22 12:54:59 +0530

    new commit

--- Backend/Controllers/posts.js
+++ Backend/Controllers/posts.js
@@ -1,24 +1,29 @@
 import PostMsg1 from "../models/PostMessage.js";
 export const getPost= async (req,res)=>
 {
-   try{
-    res.send("hello i am rendering from controlloer folder");
-    const msg= await PostMsg1.find();
-    res.status(200).json(msg);
-   }
-   catch(error)
-   {
-       res.send("getting some error");
-   }
-}
-
-export const createPost=(req,res)=>
-{
     try{
-
+    //res.send("hello i am rendering from controlloer folder");
+    const PostMsg12=await PostMsg1.find();
+     res.status(200).json(PostMsg12);
     }
     catch(error)
     {
+        res.send("getting some error while fetching the data");
+    }
 
+   
+} 
+
+export const createPost= async(req,res)=>
+{
+    const post1=req.body;
+    const newPost= new PostMsg1(post1);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+    }
+    catch(error)
+    {
+        res.send("getting some error while posting the data");
     }
 }
